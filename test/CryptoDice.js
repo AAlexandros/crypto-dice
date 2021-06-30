994f6c1cb6189403cc206b0cd25561feea17df19
aanto <aantonov@hotmail.gr>
2021-06-30 11:14:17 +0300

    Changed naming

--- test/CryptoDice.js
+++ test/CryptoDice.js
@@ -1,4 +1,4 @@
-const CryptoDice = artifacts.require("./CryptoDice.sol")
+const CryptoDice = artifacts.require("./CryptoDice_Antonov.sol")
 
 contract("Deployment tests", () => {
 	it("Should deploy smart contract properly", async() => {
